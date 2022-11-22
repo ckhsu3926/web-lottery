77a998688c25c311ea449817c0943514eb3c10c1
CKHsu <ckhsu3926@gmail.com>
2022-11-23 01:48:53 +0800

    config: ad

--- public/config.js
+++ public/config.js
@@ -1,8 +1,9 @@
 window.LotteryAdConfig = {
-  enable: false,
+  enable: true,
   href: 'https://google.com',
   imagePath: 'ad.PNG',
-  title: 'Title',
-  description: 'Description',
-  name: 'Name',
+  title: '全台瘋搶，固力伸限時買1送4',
+  description:
+    '折扣完再加碼!日本No.2固力葡萄糖胺，豪禮送不完，限時買1送3，最高送到半個月，還要給你85折',
+  name: '三得利健益1',
 };
